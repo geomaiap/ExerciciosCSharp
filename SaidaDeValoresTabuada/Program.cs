5eb21632437fc1bc1cb71a73a397ce8f1c1f4825
geomaiap <geomaia.pego@gmail.com>
2026-04-22 14:33:12 -0300

    Teste

---
+++ SaidaDeValoresTabuada/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tabuada do 9!");
+Console.WriteLine("9 x 1 = " + (9 * 1));
+Console.WriteLine("9 x 2 = " + (9 * 2));
+Console.WriteLine("9 x 3 = " + (9 * 3));
+Console.WriteLine("9 x 4 = " + (9 * 4));
+Console.WriteLine("9 x 5 = " + (9 * 5));   
+Console.WriteLine("9 x 6 = " + (9 * 6));        
+Console.WriteLine("9 x 7 = " + (9 * 7));
+Console.WriteLine("9 x 8 = " + (9 * 8));    
+Console.WriteLine("9 x 9 = " + (9 * 9));
